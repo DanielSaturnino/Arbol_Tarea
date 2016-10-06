b1f9661b69a4b3088403e0267f22df8b17fdf774
Daniel Manzano <sktosaturmx@live.com.mx>
2016-10-06 13:37:12 -0400

    creo que se hace asi

---
+++ Build/CMakeFiles/Arbol_Puzzel.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniel/Documentos/Arbol/src/ArbolP.cpp" "/home/daniel/Documentos/Arbol/Build/CMakeFiles/Arbol_Puzzel.dir/src/ArbolP.cpp.o"
+  "/home/daniel/Documentos/Arbol/src/NodoA.cpp" "/home/daniel/Documentos/Arbol/Build/CMakeFiles/Arbol_Puzzel.dir/src/NodoA.cpp.o"
+  "/home/daniel/Documentos/Arbol/src/main.cpp" "/home/daniel/Documentos/Arbol/Build/CMakeFiles/Arbol_Puzzel.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include "ArbolP.h"
+
+int main()
+{
+	std::vector<int> o;
+	for (int i = 1; i < 9; ++i)
+	{
+		o.push_back(i);
+	}
+	o.push_back(0);
+	
+	ArbolP arbol(o);
+	for (int i = 0; i < arbol.Raiz->Puzzel.size() ; ++i)
+	{	
+		if(arbol.Raiz->Puzzel[i]==0)
+			std::cout<<arbol.Raiz->Puzzel[i]<<std::endl;
+		else
+			std::cout<<(int)arbol.Raiz->Puzzel[i]<<std::endl;
+	}
+
+	std::vector<int> p;
+	p.push_back(0);
+	for (int i = 1; i < 9; ++i)
+	{
+		p.push_back(i);
+	}
+	
+
+	std::cout<<arbol.AddNodo(o,p)<<std::endl;
+
+	return 0;
+}
